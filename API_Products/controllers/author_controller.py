9699dae44b1b74ace37883c950b86631be3391ad
thiago.alexandre.new@gmail.com <SERASA@proway.treina>
2021-05-13 13:15:44 -0300

    controller_author

---
+++ API_Products/controllers/author_controller.py
@@ -0,0 +1,21 @@
+from API_Products.database import author_db, country_db
+
+
+def author_insert_validation(dict_values: dict):
+    exist_country = country_db.search_country(dict_values["country"])
+    if exist_country:
+        try:
+            text = author_db.insert_author(dict_values)
+            return dict(status=200, text=text)
+        except Exception as error:
+            return dict(status=400, text=error)
+    else:
+        dict(status=400, text="O país não foi encontrado !")
+
+
+def author_read_all_validation():
+        try:
+            authors = author_db.read_all()
+            return dict(status=200, text=authors)
+        except Exception as error:
+            return dict(status=400, text=error.args[0])
--- API_Products/database/author_db.py
+++ API_Products/database/author_db.py
@@ -1,18 +1,22 @@
-from db import DB
+from additionals.functions import convert_object_id_to_string
+from database.db import DB
 
 
 def insert_author(dict_values: dict):
 
     if dict_values:
         DB.author.insert_one(dict_values)
+        return "Registrado com sucesso !"
     else:
         raise Exception("Registros inválidos.")
 
 
 def read_all() -> list:
 
-    authors = list(DB.author.find())
-    if authors:
-        return authors
+    author = DB.author.find()
+    author_list = convert_object_id_to_string(author)
+
+    if author_list:
+        return author_list
     else:
-        raise Exception("Nenhum autor encontrado.")
+        raise Exception("Nenhum autor encontrado !")
