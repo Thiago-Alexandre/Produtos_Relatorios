0709c13bb699994cbd1d1ff97323e6561a427f97
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-13 10:16:56 -0300

    Rotina que popula a collection language implementada; Rotina que retorna uma lista de dicionários das linguas cadsatradas no banco implmentada;

---
+++ API_Products/additionals/populate_collections/populate_collection_languages.py
@@ -0,0 +1,23 @@
+from database.db import DB
+from pymongo.errors import CollectionInvalid
+
+
+def populate_languages():
+    language_list = list()
+
+    # Read a file with the list of language names and save it in db:
+    with open("./languages.txt", "r") as languages:
+        for lang in languages:
+            language_list.append(dict(name=lang.strip()))
+
+    # Save the list in the db:
+    try:
+        country = DB.language
+        country.insert_many(language_list)
+    except CollectionInvalid as error:
+        print(f"PyMongo error: {error}")
+    except Exception as error:
+        print(f"Other error ocurred: {error}")
+
+
+populate_languages()
