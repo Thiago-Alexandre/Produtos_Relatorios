7717ecf230ee747753aee23ba81fb8c315c8925d
thiago.alexandre.new@gmail.com <SERASA@proway.treina>
2021-05-13 14:01:21 -0300

    controller_authorv3

--- API_Products/controllers/author_controller.py
+++ API_Products/controllers/author_controller.py
@@ -2,15 +2,16 @@ from database import author_db, country_db
 
 
 def author_insert_validation(dict_values: dict):
-    exist_country = country_db.search_country(dict_values["country"])
-    if exist_country:
-        try:
+    try:
+        exist_country = country_db.search_country(dict_values["country"])
+        if exist_country:
             text = author_db.insert_author(dict_values)
             return dict(status=200, text=text)
-        except Exception as error:
+        else:
+            dict(status=400, text="O país não foi encontrado !")
+    except Exception as error:
             return dict(status=400, text=error)
-    else:
-        dict(status=400, text="O país não foi encontrado !")
+
 
 
 def author_read_all_validation():
