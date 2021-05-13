23acba7a1b38dbe6b993452cba740ad2b80cf0bc
Vergiliogu <54104457+Vergiliogu@users.noreply.github.com>
2021-05-13 16:15:53 -0300

    Revert "controller_author"

--- API_Products/database/author_db.py
+++ API_Products/database/author_db.py
@@ -1,22 +1,18 @@
-from additionals.functions import convert_object_id_to_string
-from .db import DB
+from db import DB
 
 
 def insert_author(dict_values: dict):
 
     if dict_values:
         DB.author.insert_one(dict_values)
-        return "Registrado com sucesso!"
     else:
         raise Exception("Registros inválidos.")
 
 
 def read_all() -> list:
 
-    author = DB.author.find()
-    author_list = convert_object_id_to_string(author)
-
-    if author_list:
-        return author_list
+    authors = list(DB.author.find())
+    if authors:
+        return authors
     else:
-        raise Exception("Nenhum autor encontrado!")
+        raise Exception("Nenhum autor encontrado.")
