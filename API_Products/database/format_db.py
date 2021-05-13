d68b74b2f2791d502b98fcc6088b24a0d2d3cf04
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-13 15:38:02 -0300

    Rotinas para popular a collection format e listar os registros do banco em uma lista de dicionários implementadas.

---
+++ API_Products/database/format_db.py
@@ -0,0 +1,16 @@
+from database.db import DB
+from pymongo.errors import CollectionInvalid, PyMongoError
+
+
+# Returns a book format list or None if nothing is find:
+def get_book_format_list() -> list or None:
+    try:
+        book_format = DB.format
+        query_result = list(book_format.find({}, {"_id": 0}))
+
+        if len(query_result) > 0:
+            return query_result
+    except CollectionInvalid as error:
+        raise Exception(f"Collection Ivalid error: {error.args[0]}")
+    except PyMongoError as error:
+        raise Exception(f"Other PyMongo error: {error.args[0]}")
