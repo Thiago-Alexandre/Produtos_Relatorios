297c414c3f7a15a3747d033e179b0dc49d44de16
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-18 09:10:44 -0300

    Conflitos com a origin/develop resolvidos;

--- API_Products/app.py
+++ API_Products/app.py
@@ -8,82 +8,82 @@ from database.auth import KEYS
 app = Flask(__name__)
 
 
-@app.route("/read_categories", methods=["GET"])
+@app.route("/categories", methods=["GET"])
 def read_categories():
     header = dict(request.headers)
-    if header["Senha"] not in KEYS:
-        return dict(text="Chave de acesso inválida."), 400
-    categories = category.read_all_categories()
-    status = categories["status"]
-    del categories["status"]
 
-    return categories, status
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        response = category.read_all_categories()
+
+    return response, response["status"]
 
 
-@app.route("/insert_categories", methods=["POST"])
+@app.route("/categories/new", methods=["POST"])
 def insert_categories():
     header = dict(request.headers)
-    if header["Senha"] not in KEYS:
-        return dict(text="Chave de acesso inválida."), 400
-    dict_values = request.get_json()
-    categories = category.insert_categories(dict_values)
-    status = categories["status"]
-    del categories["status"]
 
-    return categories, status
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        dict_values = request.get_json()
+        response = category.insert_categories(dict_values)
+
+    return response, response["status"]
 
 
-@app.route("/read_publishers", methods=["GET"])
+@app.route("/publishers", methods=["GET"])
 def read_publishers():
     header = dict(request.headers)
-    if header["Senha"] not in KEYS:
-        return dict(text="Chave de acesso inválida."), 400
-    publishers = publisher_controller.read_all_publishers()
-    status = publishers["status"]
-    del publishers["status"]
 
-    return publishers, status
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        response = publisher_controller.read_all_publishers()
+
+    return response, response["status"]
 
 
-@app.route("/insert_publishers", methods=["POST"])
+@app.route("/publishers/new", methods=["POST"])
 def insert_publishers():
     header = dict(request.headers)
-    if header["Senha"] not in KEYS:
-        return dict(text="Chave de acesso inválida."), 400
-    dict_values = request.get_json()
-    publishers = publisher_controller.insert_publisher(dict_values)
-    status = publishers["status"]
-    del publishers["status"]
 
-    return publishers, status
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        dict_values = request.get_json()
+        response = publisher_controller.insert_publisher(dict_values)
+
+    return response, response["status"]
 
 
-@app.route("/read_authors", methods=["GET"])
+@app.route("/authors", methods=["GET"])
 def read_author():
     header = dict(request.headers)
-    if header["Senha"] not in KEYS:
-        return dict(text="Chave de acesso inválida."), 400
-    authors = author_controller.author_read_all_validation()
-    status = authors["status"]
-    del authors["status"]
 
-    return authors, status
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        response = author_controller.author_read_all_validation()
+
+    return response, response["status"]
 
 
-@app.route("/insert_authors", methods=["POST"])
+@app.route("/authors/new", methods=["POST"])
 def insert_authors():
     header = dict(request.headers)
-    if header["Senha"] not in KEYS:
-        return dict(text="Chave de acesso inválida."), 400
-    dict_values = request.get_json()
-    authors = author_controller.author_insert_validation(dict_values)
-    status = authors["status"]
-    del authors["status"]
 
-    return authors, status
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        dict_values = request.get_json()
+        response = author_controller.author_insert_validation(dict_values)
+
+    return response, response["status"]
 
 
-@app.route("/insert_books", methods=["POST"])
+@app.route("/books/new", methods=["POST"])
 def insert_books():
     header = dict(request.headers)
 
@@ -98,6 +98,24 @@ def insert_books():
         insert_log_db(log_data)
     except Exception as err:
         print(err.args[0])
+
+    return response, response["status"]
+
+
+@app.route("/books", methods=["GET"])
+def read_books():
+    header = dict(request.headers)
+
+    if "Access-Key" not in list(header.keys()) or header.get("Access-Key") not in list(KEYS.values()):
+        response = dict(status=400, error="Chave de acesso inválida.", message="Verifique os dados informados.")
+    else:
+        response = book.get_book_list()
+
+    try:
+        log_data = generate_log_data(request, response)
+        insert_log_db(log_data)
+    except Exception as err:
+        print("Logging error: " + err.args[0])
 
     return response, response["status"]
 
--- API_Products/tests/test_publisher.py
+++ API_Products/tests/test_publisher.py
@@ -1,11 +1,11 @@
-from API_Products.controllers.publisher import *
+from API_Products.controllers.publisher_controller import *
 from unittest import mock, TestCase
 
 
 class TestPublisher(TestCase):
 
-    @mock.patch("API_Products.controllers.publisher.country_db")
-    @mock.patch("API_Products.controllers.publisher.publisher_db")
+    @mock.patch("API_Products.controllers.publisher_controller.country_db")
+    @mock.patch("API_Products.controllers.publisher_controller.publisher_db")
     def test_insert_publisher_works(self, mock_publisher, mock_country):
 
             mock_country.search_country.return_value = False
@@ -21,8 +21,9 @@ class TestPublisher(TestCase):
             result3 = insert_publisher(dict())
             self.assertEqual(result3, {'status': 400, 'text': 'country'})
 
+    # ===================================================================================
 
-    @mock.patch("API_Products.controllers.publisher.publisher_db")
+    @mock.patch("API_Products.controllers.publisher_controller.publisher_db")
     def test_read_all_publisher_works(self, mock_publisher):
         mock_publisher.read_all_publishers_db.return_value = []
 
@@ -32,4 +33,34 @@ class TestPublisher(TestCase):
         delattr(mock_publisher, "read_all_publishers_db")
         result2 = read_all_publishers()
         self.assertEqual(result2, dict(status=400, text="read_all_publishers_db"))
+    
+    # ===================================================================================
 
+    @mock.patch("API_Products.controllers.publisher_controller.book_db")
+    @mock.patch("API_Products.controllers.publisher_controller.publisher_db")
+    def test_update_publisher_db_works(self, mock_publisher, mock_book):
+        mock_publisher.validate_publisher.return_value = False
+        mock_publisher.update_publisher_db.return_value = "Success"
+        mock_book.update_all_publishers_book_db.return_value = "Success"
+
+        result = update_publisher(dict(name="", new_value=""))
+        self.assertEqual(result, {'status': 200, 'text': 'Success'})
+
+        mock_publisher.validate_publisher.return_value = True
+        result = update_publisher(dict(name="", new_value=""))
+        self.assertEqual(result, {'status': 400, 'text': 'O nome requerido já está sendo utilizado, não é possível atribuí-lo novamente.'})
+    
+    # ===================================================================================
+
+    @mock.patch("API_Products.controllers.publisher_controller.publisher_db")
+    def test_delete_publisher(self, mock_publisher):
+        mock_publisher.exists_publisher.return_value = False
+        mock_publisher.delete_publishers_db.return_value = "Success"
+
+        result = delete_publisher(dict())
+        self.assertEqual(result, {'status': 200, 'text': 'Success'})
+
+        mock_publisher.exists_publisher.return_value = True
+        result = delete_publisher(dict())
+        self.assertEqual(result, {'status': 400, 'text': 'A editora está sendo utilizada, não é possível deletá-la.'})
+        
