28d07b92ada8fe9800508636de017c7f4d614513
Serasa <SERASA@proway.treina>
2021-05-20 14:50:33 -0300

    Finalizado review controllers e testes

--- API_Products/controllers/author_controller.py
+++ API_Products/controllers/author_controller.py
@@ -1,34 +1,46 @@
-from database import author_db, country_db
+from database import author_db, country_db, book_db
 
 
-def author_insert_validation(dict_values: dict):
-    # Checks the body request:
+def insert_author(dict_values: dict) -> dict:
+    """
+    Método para cadastrar novo autor.
+
+    Verifica se os dados enviados são válidos. Retorna dados de erro caso estejam inválidos;
+    Verifica se o autor já está cadastrado. Retorna dados de erro caso já esteja;
+    Verifica se o país está cadastrado. Retorna dados de erro caso não esteja;
+    Salva os dados do autor. Retorna mensagem de sucesso.
+    Caso ocorra algum erro ao salvar, retorna dados de erro.
+
+    :param dict_values: {name, lastname, country}
+    :return: dict or raise Exception
+    """
     received = set(dict_values.keys())
     expected = {"name", "lastname", "country"}
-    dict_response = dict(status=400, error="Valores inseridos inválidos.", message="Verifique os dados informados.")
 
-    if not expected.issubset(received):
-        return dict_response
+    if expected.symmetric_difference(received) or not dict_values["name"] or not dict_values["lastname"] \
+            or not dict_values["country"]:
+        return dict(status=400, error="Valores inseridos inválidos.", message="Verifique os dados informados.")
 
-    validations = [
-        len(dict_values["name"]) > 0,
-        len(dict_values["lastname"]) > 0,
-        len(dict_values["country"]) > 0
-    ]
-    if not all(validations):
-        return dict_response
     try:
-        exist_country = country_db.search_country(dict_values["country"])
-        if exist_country:
-            text = author_db.insert_author_db(dict_values)
-            return dict(status=200, result_data=text)
-        else:
-            return dict(status=400, error="O país não foi encontrado!", message="Verifique o dados informados.")
+        if author_db.validate_author(dict_values):
+            return dict(status=400, error="Autor já salvo!", message="Verifique os dados informados.")
+        if not country_db.search_country(dict_values['country']):
+            return dict(status=400, error="País não foi encontrado!", message="Verifique os dados informados.")
+        authors = author_db.insert_author_db(dict_values)
+        return dict(status=200, result_data=authors)
+
     except Exception as error:
-        return dict(status=400, text=error.args[0], message="Verifique os dados informados.")
+        return dict(status=400, error=error.args[0], message="Verifique os dados informados.")
 
 
-def author_read_all_validation():
+def read_all_authors() -> dict:
+    """
+    Método para carregar os autores do banco.
+
+    Retorna os dados dos autores pesquisados no banco.
+    Caso ocorra algum erro ao pesquisar, retorna dados de erro.
+    :return: dict
+    """
     try:
         authors = author_db.read_all_authors_db()
         return dict(status=200, result_data=authors)
@@ -36,12 +48,55 @@ def author_read_all_validation():
         return dict(status=400, error=error.args[0], message="Tente novamente mais tarde.")
 
 
-def delete_author(dict_values: dict) -> dict:
+def update_author(dict_values) -> dict:
+    """
+    Método para alterar dados dos autores.
+
+    Verifica se os dados enviados são válidos. Retorna dados de erro caso estejam inválidos;
+    Verifica se o autor já está cadastrado. Retorna dados de erro caso já esteja;
+    Verifica se o país está cadastrado. Retorna dados de erro caso não esteja;
+    Altera os dados do autor nos livros caso ele esteja sendo usado;
+    Altera os dados do autor. Retorna mensagem de sucesso.
+    Caso ocorra algum erro ao alterar, retorna dados de erro.
+
+    :param dict_values: {_id, name, lastname, country}
+    :return: dict or raise Exception
+    """
+    received = set(dict_values.keys())
+    expected = {"_id", "name", "lastname", "country"}
+    if expected != received or not dict_values["_id"] or not dict_values["name"] or not dict_values["lastname"] \
+            or not dict_values["country"]:
+        return dict(status=400, error="Valores inseridos inválidos.", message="Verifique os dados informados.")
+
     try:
-        if not author_db.exist_author(dict_values):
-            authors = author_db.delete_authors_db(dict_values)
-        else:
-            raise Exception("Não foi possível deletar o autor.")
+        if author_db.validate_author(dict(name=dict_values["name"], lastname=dict_values["lastname"],
+                                          country=dict_values["country"])):
+            return dict(status=400, error="Autor já salvo!", message="Verifique os dados informados.")
+        if not country_db.search_country(dict_values['country']):
+            return dict(status=400, error="País não foi encontrado!", message="Verifique os dados informados.")
+        old_author = author_db.search_author(dict_values["_id"])
+        authors = author_db.update_author_db(dict_values)
+        book_db.update_all_authors_book_db(old_author, dict_values)
         return dict(status=200, result_data=authors)
     except Exception as error:
-        return dict(status=400, error=error.args[0], message="O autor já está sendo utilizado.")
+        return dict(status=400, error=error.args[0], message="Verifique os dados informados.")
+
+
+def delete_author(dict_values: dict) -> dict:
+    """
+    Método para excluir um autor.
+
+    Verifica se o autor está sendo utilizado em algum livro salvo. Retorna dados de erro caso esteja.
+    Realiza a exclusão do autor. Retorna mensagem de sucesso.
+    Caso ocorra algum erro ao pesquisar, retorna dados de erro.
+
+    :param dict_values: {name, lastname, country}
+    :return: dict or raise Exception
+    """
+    try:
+        if author_db.exists_author(dict_values):
+            raise Exception("O autor está sendo utilizado, não é possível deletá-lo.")
+        authors = author_db.delete_authors_db(dict_values)
+        return dict(status=200, result_data=authors)
+    except Exception as error:
+        return dict(status=400, error=error.args[0], message="Verifique os dados informados.")
--- API_Products/database/author_db.py
+++ API_Products/database/author_db.py
@@ -1,21 +1,21 @@
 from additionals.functions import convert_object_id_to_string
 from database.db import get_db
+from bson.objectid import ObjectId
 
 
 def insert_author_db(dict_values: dict):
-    db = get_db()
-    if dict_values:
+    try:
+        db = get_db()
         db.author.insert_one(dict_values)
-        return "Registrado com sucesso!"
-    else:
-        raise Exception("Registros inválidos.")
+        return "Autor salvo com sucesso!"
+    except Exception:
+        raise Exception("Ocorreu um erro ao salvar o autor.")
 
 
 def read_all_authors_db() -> list:
     db = get_db()
     author = db.author.find()
     author_list = convert_object_id_to_string(author)
-
     if author_list:
         return author_list
     else:
@@ -23,24 +23,47 @@ def read_all_authors_db() -> list:
 
 
 def delete_authors_db(dict_values: dict) -> str:
-    author_name = dict_values['name']
-    author_lastname = dict_values['lastname']
-    author_country = dict_values['country']
-
-    db = get_db()
-    affected_rows = db.author.delete_one({"name": author_name, "lastname": author_lastname, "country": author_country}).deleted_count
-
-    if affected_rows:
+    try:
+        db = get_db()
+        db.author.delete_one({"name": dict_values['name'], "lastname": dict_values['lastname'],
+                              "country": dict_values['country']})
         return "Registro excluído com sucesso!"
-    else:
+    except Exception:
         raise Exception("Nenhum autor encontrado!")
 
 
-def exist_author(dict_values) -> bool:
-    db = get_db()
+def update_author_db(dict_values: dict) -> str:
+    try:
+        db = get_db()
+        id_author = ObjectId(dict_values["_id"])
+        del dict_values["_id"]
+        db.author.update_one({"_id": id_author}, {"$set": dict_values})
+        return "Autor alterado com sucesso!"
+    except Exception:
+        raise Exception("Ocorreu um erro ao alterar o autor!")
 
+
+def exists_author(dict_values) -> bool:
+    db = get_db()
     if db.book.find_one({"author.name": dict_values["name"], "author.lastname": dict_values["lastname"],
                          "author.country": dict_values["country"]}):
         return True
-    else:
-        return False
+    return False
+
+
+def validate_author(dict_values) -> bool:
+    db = get_db()
+    validate = db.author.count_documents(dict_values, {})
+    if validate > 0:
+        return True
+    return False
+
+
+def search_author(id_author: str) -> dict:
+    db = get_db()
+    author_saved = db.author.find_one({"_id": ObjectId(id_author)})
+    author_saved["_id"] = str(author_saved["_id"])
+
+    if author_saved:
+        return author_saved
+    raise Exception("Nenhum autor encontrado!")
