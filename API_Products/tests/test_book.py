5dfae97c5adfd71e5d674175bb1d9aaf77cc334c
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-20 04:22:57 -0300

    Função search_books_for_id refatorada para search_books_by_id; Função search_book_for_id(id_book: str) removida; Função add_comment_book removida; função insert_book_validations refatorada para validate_book;

--- API_Products/tests/test_book.py
+++ API_Products/tests/test_book.py
@@ -9,7 +9,7 @@ class TestBook(TestCase):
     @mock.patch("controllers.book_controller")
     @mock.patch("controllers.book_controller.book_db")
     def test_insert_book_works(self, mock_book_db, mock_book):
-        mock_book.insert_book_validations.return_value = None
+        mock_book.validate_book.return_value = None
         mock_book_db.insert_book_db.return_value = "Success"
         mock_book_db.isbn_exists_db.return_value = False
 
@@ -23,7 +23,7 @@ class TestBook(TestCase):
     @mock.patch("controllers.book_controller")
     @mock.patch("controllers.book_controller.book_db")
     def test_verify_stock_works(self, mock_book_db, mock_book):
-        mock_book_db.search_books_for_id.return_value = [{"item_price":1, "item_quantity":10, "page_quantity":1, "format":""}]
+        mock_book_db.search_books_by_id.return_value = [{"item_price":1, "item_quantity":10, "page_quantity":1, "format": ""}]
 
         result = verify_stock([dict(quantity_purchased=20)])
         self.assertEqual(result, {'status': 400, 'books_lacking': [{'quantity_purchased': 10}], 'stocks': False})
@@ -47,16 +47,16 @@ class TestBook(TestCase):
 
     @mock.patch("controllers.book_controller.book_db")
     def test_finish_purshase_works(self, mock_book_db):
-        mock_book_db.search_books_for_id.return_value = [{"item_price":1, "item_quantity":10, "page_quantity":1, "format":""}]
+        mock_book_db.search_books_by_id.return_value = [{"item_price":1, "item_quantity":10, "page_quantity":1, "format": ""}]
 
         result = finish_purshase([], True)
         self.assertEqual(result, {'status': 400, 'text': 'Erro: reserve_quantity'})
 
-        mock_book_db.search_books_for_id.return_value = [{"item_price": 1, "item_quantity": 1, "quantity_purchased": 1, "reserve_quantity":1, "item_quantity":1}]
+        mock_book_db.search_books_by_id.return_value = [{"item_price": 1, "item_quantity": 1, "quantity_purchased": 1, "reserve_quantity":1, "item_quantity":1}]
         result = finish_purshase([{"item_quantity":1, "reserve_quantity":1, "quantity_purchased":1}], True)
         self.assertEqual(result, {'status': 200, 'text': 'Estoque alterado com sucesso!'})
 
-        mock_book_db.search_books_for_id.return_value = [{"item_price": 1, "item_quantity": 1, "quantity_purchased": 1, "reserve_quantity":1, "item_quantity":1}]
+        mock_book_db.search_books_by_id.return_value = [{"item_price": 1, "item_quantity": 1, "quantity_purchased": 1, "reserve_quantity":1, "item_quantity":1}]
         result = finish_purshase([{"item_quantity":1, "reserve_quantity":1, "quantity_purchased":1}], False)
         self.assertEqual(result, {'status': 200, 'text': 'Estoque alterado com sucesso!'})
 
@@ -64,48 +64,48 @@ class TestBook(TestCase):
     def test_insert_book_validations_works(self, mock_book_db):
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_price=0))
+            validate_book(dict(item_price=0))
         self.assertEqual("O preço deve ser maior que zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=-1, item_price=1))
+            validate_book(dict(item_quantity=-1, item_price=1))
         self.assertEqual("A quantidade de livros deve ser maior ou igual a zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=0, item_price=1, page_quantity=0))
+            validate_book(dict(item_quantity=0, item_price=1, page_quantity=0))
         self.assertEqual("A quantidade de páginas do livro deve ser maior que zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=0))
+            validate_book(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=0))
         self.assertEqual("O peso de um livro físico deve ser maior que zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=0)))
+            validate_book(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=0)))
         self.assertEqual("A altura de um livro físico deve ser maior que zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=0),))
+            validate_book(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=0),))
         self.assertEqual("O comprimento de um livro físico deve ser maior que zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=0)))
+            validate_book(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=0)))
         self.assertEqual("A largura de um livro físico deve ser maior que zero.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
-            insert_book_validations(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2025"))
+            validate_book(dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2025"))
         self.assertEqual("A data de publicação é inválida.", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
             dict_values = dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2020")
             dict_values['isbn-10'] = "111111111"
-            insert_book_validations(dict_values)
+            validate_book(dict_values)
         self.assertEqual("Formato inválido!", error.exception.args[0])
 
         with self.assertRaises(Exception) as error:
             dict_values = dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2020")
             dict_values['isbn-10'] = "111111111"
             dict_values['isbn-13'] = "111111111"
-            insert_book_validations(dict_values)
+            validate_book(dict_values)
         self.assertEqual("Formato inválido!", error.exception.args[0])
 
         mock_book_db.isbn_exists_db.return_value = True
@@ -113,7 +113,7 @@ class TestBook(TestCase):
             dict_values = dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2020")
             dict_values['isbn-10'] = "1111111111"
             dict_values['isbn-13'] = "1111111111111"
-            insert_book_validations(dict_values)
+            validate_book(dict_values)
         self.assertEqual("Isbn 10 já cadastrado.", error.exception.args[0])
         mock_book_db.isbn_exists_db.return_value = False
 
@@ -121,7 +121,7 @@ class TestBook(TestCase):
             dict_values = dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2020")
             dict_values['isbn-10'] = "1111111111"
             dict_values['isbn-13'] = "fasdfasdfdsfadsfadsfadsfadfadsf"
-            insert_book_validations(dict_values)
+            validate_book(dict_values)
         self.assertEqual("Formato inválido!", error.exception.args[0])
 
         mock_book_db.isbn_exists_db.side_effect = [False, True]
@@ -129,7 +129,7 @@ class TestBook(TestCase):
             dict_values = dict(item_quantity=0, item_price=1, page_quantity=1, format="Físico", weight=1, size=dict(height=1, lenght=1, width=1), published_at="01/10/2020")
             dict_values['isbn-10'] = "1111111111"
             dict_values['isbn-13'] = "7777777777777"
-            insert_book_validations(dict_values)
+            validate_book(dict_values)
         self.assertEqual("Isbn 13 já cadastrado.", error.exception.args[0])
 
 
