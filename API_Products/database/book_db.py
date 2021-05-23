d931cef9b56366c9468872f2a496b68d98d4dcf8
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-23 00:18:24 -0300

    Ajustes na função update_book_db; Função update_book_list_db implementada.

--- API_Products/database/book_db.py
+++ API_Products/database/book_db.py
@@ -3,7 +3,7 @@ from pymongo import ReturnDocument
 from pymongo.errors import CollectionInvalid, PyMongoError, InvalidId
 
 from additionals.functions import convert_object_id_to_string
-from database.db import get_db
+from database.db import get_db, get_conn
 
 
 def insert_book_db(dict_values: dict) -> dict or None:
@@ -19,21 +19,70 @@ def insert_book_db(dict_values: dict) -> dict or None:
         raise Exception(f"Other error: {err.args[0]}", "Erro ao salvar livro no banco de dados.")
 
 
-def update_book_db(dict_values) -> dict or None:
+def update_book_db(book_values: dict) -> dict or None:
+    """
+    This function get a book in a dictionary, update your values and returns the book before updating, or none if the
+    update fail.
+    :param book_values: the book in a dictionary.
+    :return:            the book before updating, or none if the update fail.
+    """
+
     db = get_db()
 
-    _id = dict_values["_id"]
-    del dict_values["_id"]
+    _id = book_values["_id"]
+    del book_values["_id"]
 
     try:
         before_document = db.book.find_one_and_update(
             filter={"_id": ObjectId(_id)},
-            update={"$set": dict_values},
+            update={"$set": book_values},
             upsert=False,
             return_document=ReturnDocument.BEFORE,
-            projection={key: 1 for key in list(dict_values.keys())}
+            projection={key: 1 for key in list(book_values.keys())}
         )
         return before_document
+    except PyMongoError as err:
+        raise Exception(f"PyMongo Error: {err.args[0]}")
+    except Exception as err:
+        raise Exception(f"Other error: {err.args[0]}")
+
+
+def update_book_list_db(book_list: list) -> list or None:
+    """
+    This function get a book list, update your elements and returns other list of books before updating, or None if all
+    elements are not updated.
+    :param book_list:   a list of books.
+    :return:            a list of books before updating, or None if all elements are not updated.
+    """
+
+    client = get_conn()
+    db = get_db()
+
+    book_list_with_updated_velues = []
+
+    try:
+        with client.start_session() as session:
+            with session.start_transaction():
+                for book in book_list:
+
+                    _id = book["_id"]
+                    del book["_id"]
+
+                    before_document = db.book.find_one_and_update(
+                        filter={"_id": ObjectId(_id)},
+                        update={"$set": book},
+                        upsert=False,
+                        return_document=ReturnDocument.BEFORE,
+                        projection={key: 1 for key in list(book.keys())}
+                    )
+                    if before_document:
+                        book_list_with_updated_velues.append(before_document)
+                    else:
+                        session.abort_transaction()
+                        break
+
+        if len(book_list_with_updated_velues) == len(book_list):
+            return book_list_with_updated_velues
     except PyMongoError as err:
         raise Exception(f"PyMongo Error: {err.args[0]}")
     except Exception as err:
@@ -69,7 +118,6 @@ def search_books_by_id(*args) -> list:
     db = get_db()
     book = db.book
 
-    object_id_list = []
     try:
         object_id_list = [{"_id": ObjectId(str(book_id))} for book_id in args]
     except InvalidId as err:
@@ -105,3 +153,4 @@ def update_all_authors_book_db(old_author: dict, new_author: dict) -> str:
     except Exception as error:
         print(error)
         raise Exception("Erro ao atualizar os dados dos livros!")
+
