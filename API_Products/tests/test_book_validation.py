a6b19b6b928c8f5a48011f58b283a5725990456e
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-26 10:59:22 -0300

    Testes das funções checks_dict_keys e validate_book;

--- API_Products/tests/test_book_validation.py
+++ API_Products/tests/test_book_validation.py
@@ -1,17 +1,28 @@
 from unittest import mock, TestCase
-from controllers.utils import checks_dict_keys
+from controllers.utils import checks_dict_keys, validate_book, read_all_authors_db, read_all_publishers_db, \
+    read_all_categories_db
 
 
 class TestBookValidation(TestCase):
 
     def test_checks_dict_keys(self):
         expected = {"title", "description"}
-        received = {"title", "tests"}
+        received = {"title", "description"}
 
         self.assertEqual(checks_dict_keys(expected, received, "", ""), None)
 
+        received = {"title", "testes"}
+        with self.assertRaises(Exception):
+            checks_dict_keys(expected, received, "", "")
+
     @mock.patch("controllers.utils.checks_dict_keys")
-    def test_validate_book(self):
+    @mock.patch("controllers.utils.get_book_format_list_db")
+    @mock.patch("controllers.utils.read_all_categories_db")
+    @mock.patch("controllers.utils.read_all_publishers_db")
+    @mock.patch("controllers.utils.get_language_book_list_db")
+    @mock.patch("controllers.utils.read_all_authors_db")
+    def test_validate_book(self, mock_read_all_authors_db, mock_get_language_book_list_db, mock_read_all_publishers_db,
+                           mock_read_all_categories_db, mock_get_book_format_list_db, mock_checks_dict_keys):
         book = {
             "title": "Mussum Ipsum, cacilds vidis litro abertis.",
             "description": "Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.",
@@ -19,35 +30,56 @@ class TestBookValidation(TestCase):
             "category": [
                 "Clássicos"
             ],
-            "author": [
-                {
-                    "name": "Machado de",
-                    "lastname": "Assis",
-                    "country": "Brasil"
-                }
-            ],
+            "author": [],
             "edition": 1,
             "publisher": {
-                "name": "Panda Books",
+                "name": "Novatec",
                 "country": "Brasil"
             },
-            "isbn-10": "8578887239",
-            "isbn-13": "9788578887230",
+            "isbn-10": "6666666666",
+            "isbn-13": "9999999999999",
             "format": {
                 "name": "Físico",
                 "digital": False
             },
-            "published_at": "2019-05-03",
-            "item_price": 33.94,
-            "item_cost_price": 11.50,
-            "item_quantity": 100,
+            "published_at": "2021-01-01",
+            "item_price": 99.99,
+            "item_cost_price": 66.66,
+            "item_quantity": 666,
             "size": {
                 "height": 22.8,
                 "lenght": 15.8,
-                "width": 3.2
+                "width": 3
             },
             "weight": 2,
-            "page_quantity": 368
+            "page_quantity": 666
         }
+
+        mock_read_all_authors_db.return_value = [
+            {
+                '_id': '',
+                'name': 'Mussum',
+                'lastname': 'Cacilds',
+                'country': 'Brasil'
+            }
+        ]
+        mock_get_language_book_list_db.return_value = [{'name': 'Português'}]
+        mock_read_all_publishers_db.return_value = [
+            {
+                '_id': '',
+                'name': 'Novatec',
+                'country': 'Brasil'
+            }
+        ]
+        mock_read_all_categories_db.return_value = [{'_id': '', 'name': 'Clássicos'}]
+        mock_get_book_format_list_db.return_value = [{'name': 'Físico', 'digital': False}]
+        mock_checks_dict_keys.return_value = None
+
+        book = dict()
+
+        with self.assertRaises(Exception) as err:
+            validate_book(book)
+        self.assertEqual(("Invalid body request.", "Dados do autor devem ser uma lista de dicionários."),
+                         err.exception.args)
 
 
